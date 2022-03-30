d5a22b56b15640a9203a96b5805f693d2cd5dd69
Jeison <jeiison.rodriguez7@gmail.com>
2022-03-30 13:08:56 -0500

    fin del proyecto

--- components/Buscar.js
+++ components/Buscar.js
@@ -1,12 +1,33 @@
+import { useState } from 'react'
 import styles from '../styles/Buscar.module.css'
-import Link from 'next/link'
+import Router from 'next/router';
+
 
 
 const Buscar = () => {
+
+  const [busqueda, guardarBusqueda] = useState('');
+
+  const buscarProducto = e => {
+    e.preventDefault();
+    if(busqueda.trim() === '') return
+
+    //Redireccionar a /buscar
+    Router.push({
+      pathname: '/buscar',
+      query: { q: busqueda }
+    })
+  }
+
   return (
-    <form className={styles.form}>
-        <input className={styles.input} type="text" placeholder='Buscar Productos...'/>
-        <Link href='/buscar'><a className={styles.inputsubmit}>Buscar</a></Link>
+    <form className={styles.form} onSubmit={buscarProducto}>
+        <input 
+          className={styles.input} 
+          type="text" 
+          placeholder='Buscar Productos...'
+          onChange={e => guardarBusqueda(e.target.value)}
+        />
+        <button className={styles.inputsubmit}>Buscar</button>
     </form>
   )
 }
--- pages/buscar.js
+++ pages/buscar.js
@@ -1,12 +1,50 @@
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
-
+import { useRouter } from "next/router";
+import DetallesProducto from "../components/DetallesProducto";
+import useProductos from "../hooks/useProductos";
+import styles from '../styles/Inicio.module.css'
 
 
 export default function Buscar() {
+
+
+  
+
+  const router = useRouter();
+  const {query: {q}} = router;
+
+  
+  //Todos los productos
+  const {productos} = useProductos('creado');
+  const [resultado, guardarResultado] = useState([])
+
+  useEffect(() => {
+    const busqueda = q.toLowerCase();
+    const filtro = productos.filter(producto => {
+      return(
+        producto.nombre.toLowerCase().includes(busqueda) ||
+        producto.descripcion.toLowerCase().includes(busqueda)
+      )
+    })
+    guardarResultado(filtro)
+  }, [q, productos])
+
   return (
     <div>
       <Layout>
-      <h1>Buscar</h1>
+      <div className={styles.listado}>
+        <div className={styles.contenedor}>
+          <div className={styles.datos}>
+            {resultado.map(producto =>(
+              <DetallesProducto
+                key={producto.id}
+                producto={producto}
+              />
+            ))}
+          </div>  
+        </div>
+      </div>
       </Layout>
     </div>
   )
